4945d4fafb54c545dc3f179ab90032e3893dfb26
Virendra Nakum <virendramaheshbhai92@gmail.com>
2026-06-26 10:51:21 +0530

    first project

---
+++ funcation.py
@@ -0,0 +1,20 @@
+a= float(input("gujrati marks:- ")) 
+b= float(input("hindi marks:- "))
+c= float(input("english marks:- "))
+d= float(input("economy marks:- "))
+
+e=(a+b+c+d)
+
+def added(a,b,c,d):
+ print(d)
+
+if d >90:
+    print("A+++")
+elif d>50:
+    print("A")
+else:
+    print("fail")
+
+
+added(a,b,c,d)
+print(e/4)
